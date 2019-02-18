69184778bb2cf0f9f9535a8e0913d755b2f8cba7
darinorange <OrangeHeroDavid@163.com>
2019-02-19 07:35:41 +0800

    update
    update

--- BookManager/urls.py
+++ BookManager/urls.py
@@ -1,6 +1,6 @@
 """BookManager URL Configuration
 
-The `urlpa.tterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
 Function views
