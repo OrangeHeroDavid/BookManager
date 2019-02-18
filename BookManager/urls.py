86e9bb05c1ebab1df33213ea8840b5d7f65d9da9
darinorange <OrangeHeroDavid@163.com>
2019-02-19 07:06:52 +0800

    update
    update

--- BookManager/urls.py
+++ BookManager/urls.py
@@ -17,6 +17,25 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from app01 import views
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('app01.urls')),
